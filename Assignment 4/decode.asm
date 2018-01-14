c08f2fc3fcce1e323642b234ba621a7864f4a477
xophidia <xophidia@gmail.com>
2018-01-14 18:18:59 +0100

    Update decode.asm

--- Assignment 4/decode.asm
+++ Assignment 4/decode.asm
@@ -1,6 +1,6 @@
 ; SLAE - Assignment 4: Custom encoder/decoder shellcode (Linux/x86)
 ; Author: Alain Menelet 
-; StudentID - PA-3763
+; StudentID - SLAE-3763
 ; Tested on Ubuntu 16.14.03 LTS
 
 
@@ -12,11 +12,11 @@ _start:
 	jmp short Shellcode					; jmp pop call technique
 
 decoder:
-	pop esi								; esi = @ of the shellcode
-	push esi							; save esi on the stack
+	pop esi							; esi = @ of the shellcode
+	push esi						; save esi on the stack
 	mov edi, esi						; edi = esi because we use two offset for our algo
-	xor ecx,ecx							; ecx is set to 0
-	mul ecx								; edx, eax are set to 0
+	xor ecx,ecx						; ecx is set to 0
+	mul ecx							; edx, eax are set to 0
 	
 	mov cl, 0x17						; set the length of the shellcode
 	
@@ -28,34 +28,34 @@ _:
 
 	mov byte al, [edi]					; set al with the first byte of the shellcode
 	cmp al, 0xff						; al == 0xFF ? 
-	jz impair							; if the shellcode len is odd we jump to label impair
+	jz impair						; if the shellcode len is odd we jump to label impair
 
-	mov byte dl, [edi+1]				; set dl with the second byte of the shellcode
-	sub dl, al							; we substract dl with al
-	mov byte bl, [edi+2]				; set bl with the third byte of the shellcode
-	sub bl, al							; we substract bl with al
-	xor dl, bl							; xor dl and bl
+	mov byte dl, [edi+1]					; set dl with the second byte of the shellcode
+	sub dl, al						; we substract dl with al
+	mov byte bl, [edi+2]					; set bl with the third byte of the shellcode
+	sub bl, al						; we substract bl with al
+	xor dl, bl						; xor dl and bl
 	mov [esi], dl						; then we put dl and bl decoded
 	mov [esi+1], bl
 
 
-	inc esi								; because we use esi and edi as offset, we need to increment them
+	inc esi							; because we use esi and edi as offset, we need to increment them
 	inc esi
 	inc edi
 	inc edi
 	inc edi
 
-	loop _								; loop while ecx > 0
+	loop _							; loop while ecx > 0
 	jmp short final
 	
 impair:
-	mov byte bl, [edi+1]				; set bl with the first byte after 0xFF
-	mov byte dl, [edi+2]				; set dl with the second byte after 0xFF
+	mov byte bl, [edi+1]					; set bl with the first byte after 0xFF
+	mov byte dl, [edi+2]					; set dl with the second byte after 0xFF
 	xor bl, dl
 	mov [esi], bl						; we store the decoded byte into esi
 
 final:
-	call [esp]							; we jump to the address pointed by esp, our saved esi.	
+	call [esp]						; we jump to the address pointed by esp, our saved esi.	
 
 Shellcode:
 	call decoder
